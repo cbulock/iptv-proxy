29d7a298a5cac3278988c1c501510310b2a1a737
Cameron Bulock <cameron@bulock.com>
2026-02-08 16:21:55 -0500

    fix: remove entrypoint script as it is no longer needed

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Ensure directories exist with proper permissions
-mkdir -p "${CONFIG_PATH:-/config}" "${DATA_PATH:-/data}"
-chmod 777 "${CONFIG_PATH:-/config}" "${DATA_PATH:-/data}"
-
-# Run node
-exec node index.js
